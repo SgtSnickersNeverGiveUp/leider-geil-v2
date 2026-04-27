333bde32bf861070dadfb6f50d82943d3dbbed9c
SgtSnickers <sgtsnickers1973@mail.de>
2026-04-27 19:18:30 +0200

    add config.js with SITE_CONFIG for v2

---
+++ assets/js/config.js
@@ -0,0 +1,12 @@
+window.SITE_CONFIG = {
+  // Basisdaten / Fallbacks für v2
+  discordWidgetApi: '',          // wenn du die echte Widget-URL hast, hier eintragen
+  videoPUBG: '',
+  videoARC: '',
+  eventsApi: '',
+  eventsPath: '/assets/data/events.json',
+  videosApi: '',
+  rosterApi: '/assets/data/roster.json',
+  discordRefreshInterval: 60000,
+  twitchRefreshInterval: 60000
+};
